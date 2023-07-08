adb2c2c16ec804df869a22a7c53478ffdb78e6d2
Roman Savich <savichr00@inbox.ru>
2023-07-09 00:33:04 +0300

    feat adaptive block Characters

--- Characters/script.js
+++ Characters/script.js
@@ -7,3 +7,29 @@ function func(){
 for (let elem of card) {
   elem.addEventListener('click', func);
 }
+// -----------------------------------------
+
+
+const menu = document.querySelector('.burger-menu__nav');
+const menuBtn = document.querySelector('.burger-menu__btn');
+
+const body = document.body;
+
+if (menu && menuBtn) {
+  menuBtn.addEventListener('click', function(){
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+    body.classList.toggle('lock')
+  });
+}
+
+// ----------------------------------------------
+
+document.getElementById("open-modal-btn").addEventListener("click", function(){
+  document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("close-my-modal-btn").addEventListener("click", function () {
+  document.getElementById("my-modal").classList.remove ("open")
+})
+
