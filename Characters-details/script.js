fe7c7b028e81f454a8ff0c9277b3c246049f2f43
Roman Savich <savichr00@inbox.ru>
2023-07-09 14:10:06 +0300

    feat add adaptive block Character - details

--- Characters-details/script.js
+++ Characters-details/script.js
@@ -2,3 +2,18 @@ const button = document.querySelector('.btn');
 button.addEventListener('click', function(){
   window.location.href = 'http://127.0.0.1:5500/Characters/index.html';
 });
+
+// ----------------------------
+
+const menu = document.querySelector('.burger-menu__nav');
+const menuBtn = document.querySelector('.burger-menu__btn');
+
+const body = document.body;
+
+if (menu && menuBtn) {
+  menuBtn.addEventListener('click', function () {
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+    body.classList.toggle('lock')
+  });
+}
