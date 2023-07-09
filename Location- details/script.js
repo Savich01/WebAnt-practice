ec799b763229b120e894aa68f53af08637fbe75d
Roman Savich <savichr00@inbox.ru>
2023-07-10 01:01:27 +0300

    feat the script to all blocks

--- Location- details/script.js
+++ Location- details/script.js
@@ -25,3 +25,25 @@ if (menu && menuBtn) {
     body.classList.toggle('lock')
   });
 }
+
+// --------------------
+
+document.querySelector('.serch-form__text').oninput = function () {
+  let val = this.value.trim();
+  let elasticItems = document.querySelectorAll('.card');
+  if (val != '') {
+    elasticItems.forEach(function (elem) {
+      if (elem.innerText.search(val) == -1) {
+        elem.classList.add('hide');
+      }
+      else {
+        elem.classList.remove('hide');
+      }
+    });
+  }
+  else {
+    elasticItems.forEach(function (elem) {
+      elem.classList.remove('hide');
+    });
+  }
+}
