86a2ecdbc4704d11a35ca6b8fe50fe338a61b1f9
Roman Savich <savichr00@inbox.ru>
2023-07-09 17:22:36 +0300

    feat add adaptive block Episode

--- Episodes/script.js
+++ Episodes/script.js
@@ -7,3 +7,34 @@ function func(){
 for(let elem of card) {
   elem.addEventListener('click', func);
 }
+
+// ------------------------
+
+
+const menu = document.querySelector('.burger-menu__nav');
+const menuBtn = document.querySelector('.burger-menu__btn');
+
+const body = document.body;
+
+if (menu && menuBtn) {
+  menuBtn.addEventListener('click', function () {
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+    body.classList.toggle('lock')
+  });
+}
+
+// -----------------------------------------
+
+const btn = document.querySelector('.card-box__btn');
+const cards = document.querySelectorAll('.card');
+
+btn.addEventListener('click', function () {
+
+  for (let card of cards) {
+    card.style.display = "block";
+  }
+
+  btn.style.display = "none";
+
+});
