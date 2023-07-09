4567f3089b4c95a48af0c5ef9c5b6304490b27be
Roman Savich <savichr00@inbox.ru>
2023-07-09 16:12:07 +0300

    feat add adaptive block location

--- Locations/script.js
+++ Locations/script.js
@@ -5,3 +5,46 @@ for (let elem of card) {
     window.location.href = 'http://127.0.0.1:5500/Location-%20details/';
   });
 }
+
+// ----------------------------
+
+const menu = document.querySelector('.burger-menu__nav');
+const menuBtn = document.querySelector('.burger-menu__btn');
+
+const body = document.body;
+
+if (menu && menuBtn) {
+  menuBtn.addEventListener('click', function () {
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+    body.classList.toggle('lock')
+  });
+}
+
+// --------------------------------
+
+document.getElementById("open-modal-btn").addEventListener("click", function () {
+  document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("close-my-modal-btn").addEventListener("click", function () {
+  document.getElementById("my-modal").classList.remove("open")
+})
+
+// ---------------------------------
+
+const btn = document.querySelector('.card-box__btn');
+const cards = document.querySelectorAll('.card');
+
+btn.addEventListener('click', function(){
+
+  for (let card of cards){
+      card.style.display = "block";
+  }
+
+  btn.style.display = "none";
+
+});
+
+
+
